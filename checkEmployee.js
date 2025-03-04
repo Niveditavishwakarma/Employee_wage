c198554a24d0e78264b4faaae72c47900a68cbb9
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-03-04 13:58:51 +0530

    [Nivedita] Add . refactor the code into functions

--- checkEmployee.js
+++ checkEmployee.js
@@ -1,4 +1,4 @@
-const IS_PRESENT = 1;
+const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -8,24 +8,24 @@ const WAGE_PER_HOUR = 20;
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
 
-if (empCheck === IS_PRESENT) {
-  console.log("Employee is Present");
+if (empCheck === IS_ABSENT) {
+  console.log("Employee is absent");
 } else {
-  console.log("Employee is Absent");
+  console.log("Employee is present");
 }
 
-switch (empCheck) {
-  case IS_PART_TIME:
-    empHrs = PART_TIME_HOURS;
-    break;
+function getWorkingHours(empCheck) {
+  switch (empCheck) {
+    case IS_PART_TIME:
+      return PART_TIME_HOURS;
 
-  case IS_FULL_TIME:
-    empHrs = FULL_TIME_HOURS;
-    break;
+    case IS_FULL_TIME:
+      return FULL_TIME_HOURS;
 
-  default:
-    empHrs = 0;
+    default:
+      return 0;
+  }
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
+let empWage = getWorkingHours(empCheck) * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
