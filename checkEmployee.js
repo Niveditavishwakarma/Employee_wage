5b0246455480db2874e9cc964bdce02d8826f8c0
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-03-06 11:55:52 +0530

    [Nivedita] Add . added the ability to store day, hours worked and wage earned in a single object

--- checkEmployee.js
+++ checkEmployee.js
@@ -9,8 +9,7 @@ const NUM_OF_WORKING_DAYS = 20;
 const MAX_HRS_IN_MONTH = 160;
 let totalWorkingDays = 0;
 let totalEmpHrs = 0;
-let empDailyWageMap = new Map();
-let empDailyHoursMap = new Map();
+let empDailyHrsAndWageArr = new Array();
 
 function calcDailyWage(empHrs) {
   return empHrs * WAGE_PER_HOUR;
@@ -36,33 +35,24 @@ while (
   let empHrs = getWorkingHours(empCheck);
   totalEmpHrs += empHrs;
 
-  empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
-  empDailyHoursMap.set(totalWorkingDays, empHrs);
+  empDailyHrsAndWageArr.push({
+    dayNum: totalWorkingDays,
+    dailyHours: empHrs,
+    dailyWage: calcDailyWage(empHrs),
+    toString() {
+      return (
+        "\nDay" +
+        this.dayNum +
+        " => Working Hours is " +
+        this.dailyHours +
+        " And Wage Earned = " +
+        this.dailyWage
+      );
+    },
+  });
 }
 
-const totalWage = Array.from(empDailyWageMap.values()).reduce(
-  (sum, wage) => sum + wage,
-  0
-);
-const totalHours = Array.from(empDailyHoursMap.values()).reduce(
-  (sum, hours) => sum + hours,
-  0
-);
-
 console.log(
-  `Emp Wage with Arrow:  Total hours: ${totalHours}  Total Wages: ${totalWage}`
+  "UC10 Showing Daily Hours Worked and Wage Earned: " + empDailyHrsAndWageArr
 );
-
-let nonWorkingDays = [];
-let partWorkingDays = [];
-let fullWorkingDays = [];
-
-empDailyHoursMap.forEach((hours, day) => {
-  if (hours === FULL_TIME_HOURS) fullWorkingDays.push(day);
-  else if (hours === PART_TIME_HOURS) partWorkingDays.push(day);
-  else nonWorkingDays.push(day);
-});
-
-console.log("Full Working Days: " + fullWorkingDays.join(", "));
-console.log("Part Working Days: " + partWorkingDays.join(", "));
-console.log("Non Working Days: " + nonWorkingDays.join(", "));
+r;
