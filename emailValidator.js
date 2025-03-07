b90e3fe671d8920afb2a4038fef2626e1d59e7ad
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-03-07 13:12:30 +0530

    [Nivedita] Add . Updated email validation to ensure '.' after 'bridgelabz' and mandatory 'co' part

--- emailValidator.js
+++ emailValidator.js
@@ -1,5 +1,5 @@
 function validateEmail(email) {
-  const emailPattern = /^[a-zA-Z0-9._%+-]+@bridgelabz\.[a-zA-Z]{2,}$/;
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@bridgelabz\.co(\.[a-zA-Z]{2,})?$/;
   if (emailPattern.test(email)) {
     console.log(`✅ Valid Email: ${email}`);
   } else {
@@ -8,5 +8,9 @@ function validateEmail(email) {
 }
 
 validateEmail("abc@bridgelabz.co");
-validateEmail("xyz@bridgelabz.com");
-validateEmail("test123@bridgelabz.in");
+validateEmail("xyz@bridgelabz.co");
+validateEmail("abc@bridgelabz");
+validateEmail("abc@bridgelabz.c");
+validateEmail("abc@bridgelabz.com");
+validateEmail("user@bridgelabz.co.in");
+validateEmail("test.123@bridgelabz.co.us");
